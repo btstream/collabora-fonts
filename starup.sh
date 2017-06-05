4cf0868c5c7a358cf2573a6f1b7aa4fcd9ee32e4
lzc <btstream@gmail.com>
2017-06-05 13:19:23 +0800

    Copy custom font to office's dir

--- starup.sh
+++ starup.sh
@@ -5,6 +5,5 @@ if [[ -e /opt/collaboraoffice5.3/share/fonts/custom  ]]; then
     rm -rf /opt/collaboraoffice5.3/share/fonts/custom
 fi
 
-ln -s /usr/share/fonts/Custom /opt/collaboraoffice5.3/share/fonts/custom
-
+cp -r /usr/share/fonts/Custom /opt/collaboraoffice5.3/share/fonts/custom
 bash /start-libreoffice.sh
