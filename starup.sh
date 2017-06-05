d5bbda38b7b1f71cf266260d145fb94ba7bb1898
lzc <btstream@gmail.com>
2017-06-05 15:40:32 +0800

    make an common ssl

--- starup.sh
+++ starup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # fc-cache -fv
 
+if [[ -e /opt/ssl/certs/ca ]]; then
+    export DONT_GEN_SSL_CERT=true
+fi
+
 if [[ -e /opt/collaboraoffice5.3/share/fonts/custom  ]]; then
     rm -rf /opt/collaboraoffice5.3/share/fonts/custom
 fi
