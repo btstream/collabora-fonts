1eb820a75a2b5e6fbc1f256b774b73302f35b547
lzc <btstream@gmail.com>
2017-06-05 13:05:51 +0800

    remove ssl settings of code

--- starup.sh
+++ starup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-fc-cache -fv
+# fc-cache -fv
+
+if [[ -e /opt/collaboraoffice5.3/share/fonts/custom  ]]; then
+    rm -rf /opt/collaboraoffice5.3/share/fonts/custom
+fi
+
+ln -s /usr/share/fonts/Custom /opt/collaboraoffice5.3/share/fonts/custom
+
 bash /start-libreoffice.sh
